58286f8418e097ad389f84f3e87e1b76456098be
Sourya Banerjee <souryabanerjee14@gmail.com>
2025-07-11 12:03:14 +0530

    Prediction Pipeline and App Done

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 from src.exception import CustomException
 
@@ -48,9 +49,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[model_name] = test_model_score
 
         return report
+    
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
